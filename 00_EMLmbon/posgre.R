943d87d007bbeac205652e72a7ef41d6eda702d2
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-03-13 12:51:48 -0500

    rename postgrefun to get_meta

--- 00_EMLmbon/posgre.R
+++ 00_EMLmbon/posgre.R
@@ -1,61 +1,6 @@
+# connect to metabase and query for metadata
 
-
-posgrefun <- function(dbname,
-                      host,
-                      port,
-                      user,
-                      password,
-                      dataset_ids) {
-  library(RPostgres)
-  # loads the PostgreSQL driver
-  
-  drv <- RPostgres::Postgres()
-  # creates a connection to the postgres database
-  # note that "con" will be used later in each connection to the database
-  
-  con <- dbConnect(
-    drv,
-    dbname = dbname,
-    host = host,
-    port = 5432,
-    user = user,
-    password = password
-  )
-  
-  # check for the tables
-  
-  meta <- dbReadTable(con, c("mb2eml_r", "vw_eml_attributes"))
-  fact <-
-    dbReadTable(con, c("mb2eml_r", "vw_eml_attributecodedefinition"))
-  unit <- dbReadTable(con, c("mb2eml_r", "vw_custom_units"))
-  creator <- dbReadTable(con, c("mb2eml_r", "vw_eml_creator"))
-  keyword <- dbReadTable(con, c("mb2eml_r", "vw_eml_keyword"))
-  entities <- dbReadTable(con, c("mb2eml_r", "vw_eml_entities"))
-  dataset <- dbReadTable(con, c("mb2eml_r", "vw_eml_dataset"))
-  method <- dbReadTable(con, c("mb2eml_r", "vw_eml_datasetmethod"))
-  geo <- dbReadTable(con, c("mb2eml_r", "vw_eml_geographiccoverage"))
-  tempo <- dbReadTable(con, c("mb2eml_r", "vw_eml_temporalcoverage"))
-  
-  dbDisconnect(con)
-  
-  return (
-    list(
-      meta = meta,
-      fact = fact,
-      unit = unit,
-      creator = creator,
-      keyword = keyword,
-      entities = entities,
-      dataset = dataset,
-      method = method,
-      geo = geo,
-      tempo = tempo
-    )
-  )
-  
-}
-
-postgresfun_2 <- function(dbname, host, port) {
+get_meta <- function(dbname, host, port) {
   
   # set DB driver
   library(RPostgres)
@@ -100,7 +45,7 @@ postgresfun_2 <- function(dbname, host, port) {
   # apply over list of queries and name list items
   query_dfs <- lapply(queries, param_query)
   
-  names(query_dfs) <- c("meta", "factor", "unit", "creator", "keyword", "entities", "dataset", "method", "geo", "temporal")
+  names(query_dfs) <- c("meta", "factors", "unit", "creator", "keyword", "entities", "dataset", "method", "geo", "temporal")
   
   return(query_dfs)
 }
