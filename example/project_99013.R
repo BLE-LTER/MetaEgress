1f04e86d82beb6f0b82b4b746c8ce42057e0f388
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-03-21 12:10:40 -0500

    add necessary files to example, more example documentation

--- example/project_99013.R
+++ example/project_99013.R
@@ -1,4 +1,6 @@
 # example workflow from metabase to EML document
+library(EML)
+library(RPostgres)
 
 # -----------------------
 # connect to metabase and get metadata from specified datasets
@@ -9,8 +11,8 @@ entity_meta <- get_meta(dbname = "lter_arranged", host = "localhost", port = 543
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # read in boilerplate information and license document
-boilerplate <- read_eml("../00_Shared_document/boilerplate.xml")
-license <- set_TextType("../00_Shared_document/IntellectualRights.docx")
+boilerplate <- EML::read_eml("./00_Shared_document/boilerplate.xml")
+license <- EML::set_TextType("./00_Shared_document/IntellectualRights.docx")
 
 # -----------------------
 # single entity example, datasetid 99013
@@ -19,7 +21,7 @@ license <- set_TextType("../00_Shared_document/IntellectualRights.docx")
 table_99013 <- create_entity(entity_meta, dataset_id = 99013, entity = 1)
 # create EML list object
 # outputs write_eml()-ready object
-EML_99013 <- create_EML(entity_meta, dataset_id = 99013, data_table = table_99013)
+EML_99013 <- create_EML(entity_meta, dataset_id = 99013, boilerplate = boilerplate, license = license, data_table = table_99013)
 write_eml(EML_99013, file = "EML_99013.xml")
 
 # ------------------------
@@ -32,5 +34,14 @@ write_eml(EML_99013, file = "EML_99013.xml")
 tables_99021 <- lapply(c(1, 2, 3), create_entity, meta_list = entity_meta, dataset_id = 99021)
 
 # create EML list object
-EML_99021 <- create_EML(entity_meta, dataset_id = 99021, data_table = tables_99021)
+EML_99021 <- create_EML(entity_meta, dataset_id = 99021, boilerplate = boilerplate, license = license, data_table = tables_99021)
+
+# validate and serialize (write) EML document
+eml_validate(EML_99021)
+
+# which returns
+# [1] FALSE
+# attr(,"errors")
+# [1] "Element 'abstract': This element is not expected. Expected is one of ( references, alternateIdentifier, shortName, title )."
+
 write_eml(EML_99021, file = "EML_99021.xml")
