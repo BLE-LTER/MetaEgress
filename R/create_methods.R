fb56745e39879de0302b1d1e0bf71ba9d98774a3
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-08-05 13:50:07 -0500

    create_method_step: get provenance
    - get data source EML and insert into appropriate slot
    - concatenate auto-generated description with methodStep description

--- R/create_methods.R
+++ R/create_methods.R
@@ -7,11 +7,23 @@
 
 create_method_section <-
   function(meta_list, dataset_id, file_dir) {
+    method_desc <-
+      subset(meta_list[["methodstep"]], datasetid == dataset_id)
+    provenance <-
+      subset(meta_list[["provenance"]], datasetid == dataset_id)
+    # protocols <- 
+    #  subset(meta_list[["protocols"]], datasetid == dataset_id)
     
-    method_desc <- subset(meta_list[["methodstep"]], datasetid == dataset_id)
-    provenance <- subset(meta_list[["provenance"]], datasetid == dataset_id)
     
-    methodStep <- lapply(method_desc["methodstep_id"], create_method_step, meta_list, method_desc = method_desc, provenance = provenance, file_dir = NULL)
+    methodStep <-
+      lapply(
+        method_desc[["methodstep_id"]],
+        create_method_step,
+        meta_list,
+        method_desc = method_desc,
+        provenance = provenance,
+        file_dir = NULL
+      )
     
     names(methodStep) <- NULL
     
@@ -25,39 +37,57 @@ create_method_section <-
 #'
 #'
 
-create_method_step <- 
-  function(meta_list, method_desc, provenance, file_dir = NULL, step_id){
+create_method_step <-
+  function(step_id,
+           meta_list,
+           method_desc,
+           provenance,
+           file_dir = NULL) {
     # ---
     # get method step description
-    #description <-
-    #  if (is.null(file_dir))
-    #    set_TextType(method_desc["methodstep_id" == step_id, "description"])
-    #else
-    #  set_TextType(file.path(file_dir, method_desc[["description"]]))
+    description <-
+      if (is.null(file_dir))
+        set_TextType(subset(method_desc, methodstep_id == step_id, description)[[1]])
+    else
+      set_TextType(file.path(file_dir, subset(method_desc, methodstep_id == step_id, description)[[1]]))
     
     # ---
     # get and expand provenance
-    provenance <- subset(provenance, "methodstep_id" == step_id, "data_source_packageId")
     
-    data_source <- list()
-    for (i in 1:length(provenance)) {
-      prov_xml <- EDIutils::api_get_provenance_metadata(provenance[[i]])
-      prov_xml <- emld::as_emld(prov_xml)
-      data_source <- list(data_source, dataSource = prov_xml[["dataSource"]])
+    if (nrow(provenance) > 0) {
+    ids <-
+      subset(provenance, methodstep_id == step_id, data_source_packageId)[[1]]
+    
+    prov <-
+      lapply(lapply(ids, EDIutils::api_get_provenance_metadata),
+             emld::as_emld)
+    
+    data_source <- lapply(prov, `[[`, "dataSource")
+    
+    # what a roundabout way to do this
+    prov_desc <- set_TextType(text = paste0(unlist(lapply(prov, `[[`, "description"), use.names = F), collapse = " \n "))
+    
+    # ---
+    # stitch descriptions together
+    description$para <- c(description$para, prov_desc)
+    
+    } else {
+      data_source <- NULL
     }
     
-    return(list(
-      description = description,
-      dataSource = data_source
-    ))
+    
+    return(list(description = description,
+      dataSource = data_source))
     
     # ---
     # get protocols
     
     
+    
+    
     # ---
     # get instruments
-
+    
     
     # ---
     # get software
