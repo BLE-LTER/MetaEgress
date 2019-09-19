1d25d9f111c59a37e141493cb15799b3868a3186
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-09-19 13:35:30 -0500

    pad coordinates to 6 decimal points
    plus some naming changes

--- R/assemble_coverage.R
+++ R/assemble_coverage.R
@@ -1,38 +1,40 @@
 #' @title Assemble dataset coverage elements
-#' 
+#'
 #' @description Assemble geographic, temporal, and taxonomic coverage elements at the dataset level.
-#' 
+#'
 #' @param meta_list (list) A list of dataframes containing metadata returned by \code{\link{get_meta}}.
-#' 
+#'
 #' @return (list) A named list containing geographicCoverage, temporalCoverage, and taxonomicCoverage elements, each NULL if no information is provided.
 #' 
-#' @export 
+#' @export
 
 assemble_coverage <- function(meta_list) {
-  
   geo <- meta_list[["geo"]]
   
   if (nrow(geo) > 0) {
-    geoall <- apply(geo, 1, assemble_geographic)
+    geocov <- apply(geo, 1, assemble_geographic)
     names(geoall) <- NULL
-  } else geoall <- NULL
+  } else
+    geocov <- NULL
   
   tempo <- meta_list[["temporal"]]
   
   if (nrow(tempo) > 0) {
-    tempall <- apply(tempo, 1, assemble_temporal)
-  } else tempall <- NULL
+    tempcov <- apply(tempo, 1, assemble_temporal)
+  } else
+    tempcov <- NULL
   
   taxa <- meta_list[["taxonomy"]]
   
   if (nrow(taxa) > 0) {
     taxcov <- assemble_taxonomic(taxa)
-  } else taxcov <- NULL
+  } else
+    taxcov <- NULL
   
   coverage <-
     list(
-      geographicCoverage = geoall,
-      temporalCoverage = tempall,
+      geographicCoverage = geocov,
+      temporalCoverage = tempcov,
       taxonomicCoverage = taxcov
     )
   return(coverage)
@@ -41,26 +43,25 @@ assemble_coverage <- function(meta_list) {
 # ------------------------------------------------------------------------------
 
 assemble_temporal <- function(tempo_row) {
-  
-    tempcover <-
-      list(rangeOfDates = list(
-        beginDate = list(calendarDate = as.character(tempo_row[["begindate"]])),
-        endDate = list(calendarDate = as.character(tempo_row[["enddate"]]))
-      ))
-    return(tempcover)
+  tempcov <-
+    list(rangeOfDates = list(
+      beginDate = list(calendarDate = as.character(tempo_row[["begindate"]])),
+      endDate = list(calendarDate = as.character(tempo_row[["enddate"]]))
+    ))
+  return(tempcov)
 }
 
 # ------------------------------------------------------------------------------
 
 assemble_geographic <- function(geo_row) {
-  geo <-
+  geocov <-
     list(
       geographicDescription = geo_row[["geographicdescription"]],
       boundingCoordinates = list(
-        westBoundingCoordinate = as.character(geo_row[["westboundingcoordinate"]]),
-        eastBoundingCoordinate = as.character(geo_row[["eastboundingcoordinate"]]),
-        northBoundingCoordinate = as.character(geo_row[["northboundingcoordinate"]]),
-        southBoundingCoordinate = as.character(geo_row[["southboundingcoordinate"]]),
+        westBoundingCoordinate = as.character(format(geo_row[["westboundingcoordinate"]], nsmall = 6)),
+        eastBoundingCoordinate = as.character(format(geo_row[["eastboundingcoordinate"]], nsmall = 6)),
+        northBoundingCoordinate = as.character(format(geo_row[["northboundingcoordinate"]], nsmall = 6)),
+        southBoundingCoordinate = as.character(format(geo_row[["southboundingcoordinate"]], nsmall = 6)),
         boundingAltitudes = list(
           altitudeMinimum = null_if_na(geo_row, "altitudeminimum"),
           altitudeMaximum = null_if_na(geo_row, "altitudemaximum"),
@@ -68,14 +69,15 @@ assemble_geographic <- function(geo_row) {
         )
       )
     )
-  return(geo)
+  return(geocov)
 }
 
 # ------------------------------------------------------------------------------
 
 assemble_taxonomic <- function(taxa) {
   if (nrow(taxa) > 0) {
-    taxcov <- set_taxonomicCoverage(taxa[["taxonrankvalue"]], expand = T)
+    taxcov <-
+      set_taxonomicCoverage(taxa[["taxonrankvalue"]], expand = T)
     names(taxcov[[1]]) <- NULL
   } else {
     taxcov <- NULL
